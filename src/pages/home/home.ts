ee6c487f2760d044f938a7a690515058dded26a6
aplikasi <aplikasi@TIPDAs-MacBook-Air.local>
2018-04-04 19:30:46 +0700

    ionic geolocation

---
+++ src/pages/home/home.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  lat: any;
+  lng: any;
+  constructor(public navCtrl: NavController, public geo: Geolocation) {
+
+  }
+
+  ionViewDidLoad(){
+    this.geo.getCurrentPosition().then( pos => {
+      this.lat = pos.coords.latitude;
+      this.lng = pos.coords.longitude;
+    }).catch( err => console.log(err));
+  }
+  
+}
